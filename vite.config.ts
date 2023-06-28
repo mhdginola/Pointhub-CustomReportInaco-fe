3a391c5f0ceda3903f5db5559e3fa1276d3b8de4
Ginola <mhdginola@gmail.com>
2023-06-28 09:52:33 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+import vue from '@vitejs/plugin-vue'
+import envCompatible from 'vite-plugin-env-compatible'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [UnoCSS(), vue(), envCompatible()],
+  envPrefix: "VITE_",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
