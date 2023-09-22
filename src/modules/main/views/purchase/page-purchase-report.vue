7f820677d8f2095a6ae75247272edd3bd81eab60
Ali Akbar <ali5alkaf5@gmail.com>
2023-09-22 21:14:33 +0700

    Fixed Purchase Report

--- src/modules/main/views/purchase/page-purchase-report.vue
+++ src/modules/main/views/purchase/page-purchase-report.vue
@@ -8,7 +8,7 @@ const columns = [
     // {name: 'invoiceNumber', label: 'Purchase Invoice'},
     {name: 'supplier', label: 'Supplier', func: (d: any)=>d.supplier?.name},
     // {name: 'invoiceNumber', label: 'No. Faktur'},
-    {name: 'noSuratJalan', label: 'No. Surat Jalan'},
+    {name: 'noSuratJalan', label: 'No. Surat Jalan', func: (d: any) => d.purchaseReceive?.number ?? '-'},
     {name: 'noFakturPajak', label: 'No. Faktur Pajak'},
     {name: 'taxBase', label: 'DPP', type: 'number', func: (d: any) => Math.round(parseFloat(d.taxBase) * 1000) / 1000},
     {name: 'tax', label: 'PPN', type: 'number', func: (d: any) => Math.round(parseFloat(d.tax) * 1000) / 1000},
